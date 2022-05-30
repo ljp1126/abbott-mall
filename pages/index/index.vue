8a59ca6ac1bfbe2da73a1ef519f5d797476fbadf
Labbott <1006762873@qq.com>
2022-05-30 21:46:20 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<!--
+ * @Description: 
+ * @Author: lijiapeng
+ * @Date: 2022-05-23 22:49:58
+ * @LastEditTime: 2022-05-30 21:29:07
+ * @LastEditors: lijiapeng
+ * @Reference: 
+-->
+<template>
+	<view class='index'>
+			
+		<view class='f-active-color'> 文字  </view>
+		
+		<view class='iconfont icon-xiaoxi'>  出来了吗?  </view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+.wx-nav{
+	text-align: center;
+	height: 200rpx;
+	width:100%;
+	line-height: 200rpx;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+</style>
