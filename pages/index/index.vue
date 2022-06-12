a3d2c3771d9fa94cba15e89046f4ac205af4fd7b
Labbott <1006762873@qq.com>
2022-06-12 14:08:34 +0800

    '商品列表封装'

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,17 @@
+<!--
+ * @Description: 
+ * @Author: lijiapeng
+ * @Date: 2022-05-30 21:31:48
+ * @LastEditTime: 2022-06-12 10:52:35
+ * @LastEditors: lijiapeng
+ * @Reference: 
+-->
 <template>
 	<view class='index'>
-    
     <IndexSwiper></IndexSwiper>
     <Recommend></Recommend>
-		
+    <Card cardTitle="猜你喜欢"></Card>
+    <CommodityList></CommodityList>
 	</view>
 </template>
 
@@ -11,6 +19,8 @@
 <script>
   import IndexSwiper from '@/components/index/IndexSwiper.vue'
   import Recommend from '@/components/index/Recommend.vue'
+  import Card from '@/components/common/Card.vue'
+  import CommodityList from '@/components/common/CommodityList.vue'
 	export default {
 		data() {
 			return {
@@ -19,7 +29,9 @@
 		},
     components:{
       IndexSwiper,
-      Recommend
+      Recommend,
+      Card,
+      CommodityList
     },
 		onLoad() {
 
